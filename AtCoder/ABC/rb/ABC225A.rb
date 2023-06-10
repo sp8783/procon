ecd174b363b36db97eae14f31a8b6484cb70416d
sp8783 <p.sp8783@gmail.com>
2023-06-11 04:54:59 +0900

    add another solution

--- AtCoder/ABC/rb/ABC225A.rb
+++ AtCoder/ABC/rb/ABC225A.rb
@@ -1,3 +1,8 @@
-n = gets.chomp.chars.uniq.size
+# uniq.sizeを使う
+# n = gets.chomp.chars.uniq.size
+# puts n == 3 ? 6 : n == 2 ? 3 : 1
 
+# tallyを使う
+l = gets.chomp.chars
+n = l.tally.values.size
 puts n == 3 ? 6 : n == 2 ? 3 : 1
